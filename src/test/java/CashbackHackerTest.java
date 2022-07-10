2c29d3260434d2e1dc018a28a2d4c0a601e2ed4c
Алексей Полетаев <Alexkok18@mail.ru>
2022-07-10 12:11:42 +0300

    add test for CashbackHackerTest

--- src/test/java/CashbackHackerTest.java
+++ src/test/java/CashbackHackerTest.java
@@ -1,10 +1,51 @@
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
-class CashbackHackerTest {
+public class CashbackHackerTest {
+    CashbackHacker service = new CashbackHacker();
 
     @Test
-    void remain() {
+    public void shouldGetRemains200() {
+
+        int amount = 800;
+
+        int actual = service.remain(amount);
+        int expected = 200;
+
+        assertEquals(actual, expected);
     }
+
+    @Test
+    public void shouldGetRemains1() {
+
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldGetRemains999() {
+
+        int amount = 1;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldGetRemains0() {
+
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
 }
